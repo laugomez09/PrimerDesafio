9ceff0d152e8422eb24965bda78c9c94290e4e0e
Lautaro Thomas Gomez Muñoz <lautarothomasgomezmunoz@gmail.com>
2023-11-10 01:14:19 -0300

    Segundo desafio!

--- Desafios/desafio1y2.js
+++ Desafios/desafio1y2.js
@@ -1,12 +1,24 @@
-class ProductManager {
+const fs = require("node:fs");
 
-	constructor() {
-		this.products = [];
-	}
+class ProductManager {
 
 	static id = 0;
 
-	addProduct(title, description, price, thumbnail, code, stock) {
+	constructor(fileName) {
+		this.path = fileName;
+		if (fs.existsSync(this.path)) {
+			try {
+				const fileText = fs.readFileSync(this.path, "utf-8");
+				this.products = JSON.parse(fileText);
+			} catch {
+				this.products = [];
+			}
+		} else {
+			this.products = [];
+		}
+	}
+
+	async addProduct(title, description, price, thumbnail, code, stock) {
 		if (!title || !description || !price || !thumbnail || !code || !stock) {
 			console.log("Error: Tienes que completar todos los campos obligatoriamente.");
 		} else {
@@ -15,6 +27,7 @@ class ProductManager {
 			} else {
 				ProductManager.id++
 				this.products.push({ title, description, price, thumbnail, code, stock, id: ProductManager.id });
+				await this.saveFile();
 			}
 		}
 	}
@@ -32,11 +45,43 @@ class ProductManager {
 		}
 	}
 
+
+	async saveFile() {
+		try {
+			await fs.promises.writeFile(
+				this.path,
+				JSON.stringify(this.products, null, "\t"),
+				"utf-8"
+			);
+		} catch (error) {
+			console.log(`ERROR ${error}`);
+		}
+	}
+
+	async deleteProduct(id) {
+		const productElimined = this.products.find((p) => p.id == id);
+
+		if (productElimined) {
+			const newProductsArray = this.products.filter((p) => p.id != id);
+
+			this.products = newProductsArray;
+
+			await this.saveFile();
+		} else {
+			console.log("¡ERROR!");
+		}
+	}
 }
+
+
 
 const productos = new ProductManager;
 
 productos.addProduct("Coca-Cola", "Es una gaseosa azucarada y vendida a nivel mundial", 950, "https://coca-colafemsa.com/wp-content/uploads/2020/02/1-40.png", 45454, 10)
 productos.addProduct("Cotonetes", "es un instrumento utilizado para recoger muestras, para su posterior estudio, normalmente en medicina se usa para saber que germen afecta a una infección, también se usa en cosméticos y aunque también se suele usar en la limpieza de la oreja", 400, "https://tekielar.vtexassets.com/arquivos/ids/168479/6030941.jpg?v=637975102718330000", 23435, 9)
+
+console.log(productos.getProducts())
+
+productos.deleteProduct(1)
 
 console.log(productos.getProducts())
